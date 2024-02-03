5a1e137b3c0120d32c082769a03aa38cca35ecbc
Patryk <pskoczylas28@gmail.com>
2024-02-03 22:04:51 +0100

    added new modules + new styling

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const inputArea = document.querySelector('#input');
 const inputAreaDup = document.querySelector('#input2');
 const inputAreaDupDup = document.querySelector('#input3');
+const inputForSum = document.querySelector('#input4');
 const outputArea = document.querySelector('#output');
 const outputAreaDup = document.querySelector('#output2');
+const outputForSum = document.querySelector('#output3');
 const submitButton = document.querySelector('#submit');
 const submitButton2 = document.querySelector('#submit2');
+const submitButton3 = document.querySelector('#submit3');
 const outputLabel = document.querySelector('#outputlabel');
+const sumOutput = document.querySelector('#sumoutput');
 const regex = /\d{10}/;
+let dup = 0;
 
-function convertInputToInvoices(input) {
+function convertInputToInvoices(input, output) {
 	const regex = /\d{10}/;
 	const final = [];
 	let text = input.value.split(' ');
@@ -38,26 +43,35 @@ function convertInputToInvoices(input) {
 	}
 
 	const unique = [...new Set(final)];
-	outputArea.value = ' ';
+	// outputArea.value = ' ';
 
 	for (let i = 1; i < unique.length; i++) {
 		unique[i] = ' ' + unique[i];
 	}
+	output.value = unique;
+	dup = final.length;
+
 	return unique;
 }
 
 function submitButtonHandler() {
-	let unique = convertInputToInvoices(inputArea);
-	outputArea.value = convertInputToInvoices(inputArea);
-
-	outputLabel.textContent = 'Output: ' + unique.length + ' invoices' /*+ '( ' + final.length + ' with duplicates)'*/;
+	convertInputToInvoices(inputArea, outputArea);
+	let unique = convertInputToInvoices(inputArea, outputArea);
+	outputArea.value = convertInputToInvoices(inputArea, outputArea);
+	let duplicates = dup - unique.length;
+	outputLabel.textContent =
+		'Output: ' +
+		unique.length +
+		' invoices (' +
+		duplicates +
+		' duplicates removed)';
 	inputAreaDup.textContent = outputArea.value;
 }
 function findMissing() {
 	let more;
 	let less;
 	let missing = [];
-	inputAreaDupDup.value = convertInputToInvoices(inputAreaDupDup);
+
 
 	if (inputAreaDup.value.length > inputAreaDupDup.value.length) {
 		more = inputAreaDup.value;
@@ -73,15 +87,72 @@ function findMissing() {
 	less = less.split(' ');
 	less = less.join();
 	less = less.split(',');
-	console.log(typeof more);
-	console.log(less);
 	more.forEach(element => {
 		if (!less.includes(element)) {
 			missing.push(element);
 		}
 	});
+	for (let i = 1; i < missing.length; i++) {
+		missing[i] = ' ' + missing[i];
+	}
+
 	outputAreaDup.value = missing;
 }
 
+function copyToClipboard() {
+	outputArea.classList.add('copyanimation');
+	navigator.clipboard.writeText(outputArea.value);
+	setTimeout(() => {
+		outputArea.classList.remove('copyanimation');
+	}, 1000);
+}
+function copyToClipboardTwo() {
+	outputAreaDup.classList.add('copyanimation');
+	navigator.clipboard.writeText(outputAreaDup.value);
+	setTimeout(() => {
+		outputAreaDup.classList.remove('copyanimation');
+	}, 1000);
+}
+
+function convertBoth() {
+	convertInputToInvoices(inputAreaDup, inputAreaDup);
+	convertInputToInvoices(inputAreaDupDup, inputAreaDupDup);
+	findMissing();
+}
+
+function sumInvoices() {
+	const inv = [];
+	console.log(inputForSum.value);
+	let text = inputForSum.value.split('\n');
+	text.splice(0, 22);
+	text.pop();
+	text.pop();
+	text.pop();
+	console.log(text);
+
+	class Invoices {
+		constructor(number, due, amount) {
+			this.number = number;
+			this.due = due;
+			this.amount = amount;
+		}
+	}
+	console.log(text[6]);
+	for (let i = 0; i < text.length; i += 10) {
+		inv.push(new Invoices(text[i], text[i + 6], parseFloat(text[i + 7].replace(/[^0-9.]+/g, ''))));
+	}
+	console.log(inv);
+	let sum = 0;
+	inv.forEach(element => {
+		outputForSum.textContent += element.number + '  ' + element.due + '  ' + element.amount + '\n';
+		const regex = /\d/;
+		sum += element.amount;
+	});
+	sumOutput.textContent = 'Sum: ' + sum.toFixed(2);
+}
+
 submitButton.addEventListener('click', submitButtonHandler);
-submitButton2.addEventListener('click', findMissing);
+submitButton2.addEventListener('click', convertBoth);
+submitButton3.addEventListener('click', sumInvoices);
+outputArea.addEventListener('click', copyToClipboard);
+outputAreaDup.addEventListener('click', copyToClipboardTwo);
